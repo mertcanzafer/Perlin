c171734f2683256405dd8547addeeb24f9554ac2
mrtzfr <mertcanzafer06@gmail.com>
2025-07-01 22:00:27 +0300

    Perlin ASSERT imp.

--- src/Exception.cpp
+++ src/Exception.cpp
@@ -2,4 +2,11 @@
 
 namespace dbg {
 	SDL_Exception::SDL_Exception(const std::string& message) : std::runtime_error(message + "\n" + SDL_GetError()){}
+	
+	void reportAssertionFailure(const char* expression, const char* file, int line)
+	{
+		std::cerr << "Assertion failed: " << expression
+			<< " in file " << file <<
+			" at line " << line << std::endl;
+	}
 }
--- src/Exception.h
+++ src/Exception.h
@@ -10,4 +10,27 @@ namespace dbg {
         explicit SDL_Exception(const std::string& message);
     };
 
+    void reportAssertionFailure(const char* expression, const char* file, int line);
+    
+#if defined(_MSC_VER)
+#define dbg_debugBreak() __debugbreak()
+#elif defined(__GNUC__) || defined(__clang__)
+#define dbg_debugBreak() __builtin_trap()
+#else
+#define dbg_debugBreak() ((void)0)
+#endif
 }
+
+#ifdef ASSERTIONS_ENABLED
+
+#define P_ASSERT(expr)                                            \
+        do {                                                          \
+            if (!(expr)) {                                            \
+                dbg::reportAssertionFailure(#expr, __FILE__, __LINE__); \
+                dbg_debugBreak();                                     \
+            }                                                         \
+        } while (0)
+
+#else
+#define P_ASSERT(expr) ((void)0)
+#endif
